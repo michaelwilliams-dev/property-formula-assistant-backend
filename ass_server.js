2016cf7fbd07745804b13a54dd10889e7d845fa1
michaelwilliams-dev <michael@gainfield.co.uk>
2025-07-31 12:42:11 +0100

    Update ass_server.js

--- ass_server.js
+++ ass_server.js
@@ -13,6 +13,7 @@ import { loadIndex, searchIndex } from './vector_store.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
